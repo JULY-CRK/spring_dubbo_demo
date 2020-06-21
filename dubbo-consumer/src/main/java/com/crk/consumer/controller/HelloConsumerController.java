cbc6735fb07b81c2cc39e8c92114bf3472471ab9
1043181912@qq.com <1043181912@qq.com>
2020-06-21 08:28:04 +0800

    the first commit for for branch

--- dubbo-consumer/src/main/java/com/crk/consumer/controller/HelloConsumerController.java
+++ dubbo-consumer/src/main/java/com/crk/consumer/controller/HelloConsumerController.java
@@ -15,6 +15,7 @@ public class HelloConsumerController {
     public String sayHello(@PathVariable("name") String name){
         int i = 0;
         for(int j = 0;j < 10;j++){
+            System.out.println("this is a for branch");
             i++;
         }
 
